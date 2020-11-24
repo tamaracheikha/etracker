444cf8bbe836bb6cc99220b3e9fed014b853cf1c
Megan Wong <meganwong117@gmail.com>
2020-11-24 14:58:19 +0000

    finished editing columns for apps index

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,10 +1,10 @@
 class Application < ApplicationRecord
   belongs_to :user
-  
+
   has_one_attached :cv
   has_one_attached :cover_letter
   has_many :status_updates, dependent: :destroy
   has_many :notes, dependent: :destroy
-  STATUSES = ["Saved", "Applied", "Interviewing", "Offer"]
+  STATUSES = ["Saved", "Applied", "Interviewing", "Offer", "Rejected"]
   validates :application_status, inclusion: { in: STATUSES }
 end
