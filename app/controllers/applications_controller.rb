3c800154049fcb4a698f3dab119d08642f28eee3
Megan Wong <meganwong117@gmail.com>
2020-11-24 12:16:06 +0000

    add index page and controller  actions

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationsController < ApplicationController
+  def index
+    @applications = Application.all
+    @statuses = ["Saved", "Applied", "Interviewing", "Offer"]
+  end
 end
--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,6 @@
 class Application < ApplicationRecord
   belongs_to :user
   has_many :status_updates, :notes
+  STATUSES = ["Saved", "Applied", "Interviewing", "Offer"]
+  validates :application_status, inclusion: { in: STATUSES }
 end
