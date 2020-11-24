fb1044389e9ebf45ff4868690120aea9af9fa3fd
zineb alami <alamizineb0@gmail.com>
2020-11-24 13:05:30 +0100

    job-page

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,4 +1,22 @@
 class ApplicationsController < ApplicationController
+  def new
+    @application = Application.new
+  end
+
+  def create
+    @application = Application.new(application_params)
+    @application.user = current_user
+    if @application.save
+      redirect_to_application_path(@application)
+    else
+      render :new
+    end
+  end
+
+  def application_params
+    params.require(:application).permit(:company_name, :company_link, :location, :job_title, :job_description, :cv, :cover_letter)
+  end
+
   def show
     @application = Application.find(params[:id])
   end
