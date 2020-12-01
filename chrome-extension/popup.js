068723cd31fcc1e8754232898c6469347c91ad99
Noemie Lachman <noemie.lachman@hotmail.fr>
2020-12-01 12:33:59 +0000

    extension

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,13 +1,14 @@
 function listenClick() {
-  const button = document.getElementById('send-data');
-  button.addEventListener('click', () => {
-    chrome.tabs.executeScript({
-      file: 'scripts/send-data.js'
-    });
-  })
+  // const button = document.getElementById('send-data');
+  // button.addEventListener('click', () => {
+  //   chrome.tabs.executeScript({
+  //     file: 'scripts/send-data.js'
+  //   });
+  // })
   // console.log("hi")
   // const form = document.getElementById('form-extension');
-  // button.addEventListener('submit', () => {
+  // form.addEventListener('submit', () => {
+  //   console.log('hi')
   //   chrome.tabs.executeScript({
   //     file: 'scripts/send-data.js'
   //   });
